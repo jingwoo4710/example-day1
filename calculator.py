3a0b727c6c2f895ac4a5ce6fa83877aafa2a8aff
jingwoo4710 <jaewoo4710@gmail.com>
2020-11-09 15:40:53 +0900

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def sum (num1, num2):
+    return num1 + num2
+
+def sub (num1, num2):
+    return num1 - num2
+
+def mul (num1, num2):
+    return num1 * num2
+
+def div(num1, num2):
+    return num1 / num2
+
+print(div(1,2))
